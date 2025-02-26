41f649befc2e8b5ee895e417765947abe7c146fa
YoniIfrah <yonifrah996@gmail.com>
2025-02-26 18:51:04 +0200

    added connString

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -8,6 +8,9 @@ using GameStore.Api.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
 app.MapGameEndpoints();
